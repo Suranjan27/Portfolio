93fff1069383cb004ed83537c0be66c3c7f04293
Suranjan Sengupta <146031283+Suranjan27@users.noreply.github.com>
2026-04-26 02:34:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // ADD THIS LINE! Replace with your exact GitHub repo name:
-  base: '/Portfolio/', 
+  base: '/git remote set-url origin https://github.com/Suranjan27/Portfolio.git/', 
 })
