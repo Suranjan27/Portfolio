e01e73517f266ff61216396ddba16d3c13c5bfa5
Suranjan Sengupta <aec.ece.suranjansengupta.2026.083@gmail.com>
2026-04-26 01:54:06 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADD THIS LINE! Replace with your exact GitHub repo name:
+  base: '/Portfolio/', 
 })
