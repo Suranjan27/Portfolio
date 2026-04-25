3bf82dc0bde9066d5b5f9bd0f0248831dd4acb80
Suranjan Sengupta <146031283+Suranjan27@users.noreply.github.com>
2026-04-26 02:42:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ADD THIS LINE! Replace with your exact GitHub repo name:
-  base: '/Portfolio/', 
+  // Change this from '/Portfolio/' to just '/'
+  base: '/', 
 })
